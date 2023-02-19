98823dde3c76ac84ac6b518c309897b50cca2210
NagarRasyid <nagar.rasyid@gmail.com>
2023-02-19 22:08:39 +0700

    Tugas PW pertemuan Kedua

---
+++ tugas1/1c.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+    <style>
+        body {
+            font-family: arial;
+        }
+        table {
+            margin: 20px;
+        }
+        tr {
+            font-size: 15px; 
+            border-style: groove;
+        }
+        td {
+            border: 1px solid;
+            background-color: brown;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            padding-left: 15px;
+            padding-right: 15px;
+        }
+    </style>
+</head>
+<body>
+    <table cellspacing="0" cellpadding="5">
+        <tr>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+        </tr>
+    </table>
+</body>
+</html>
